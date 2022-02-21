e6531418f414d11ab420acbfd45e03bdb8da5b98
Dennis Buduev <dbuduev@users.noreply.github.com>
2022-02-21 16:13:57 +1300

    test: Unit tests having external cerbos
    Signed-off-by: Dennis Buduev <“dbuduev@users.noreply.github.com”>

--- ent-adapter/db/adapter_test.go
+++ ent-adapter/db/adapter_test.go
@@ -9,6 +9,10 @@ import (
 	responsev1 "github.com/cerbos/cerbos/api/genpb/cerbos/response/v1"
 	"google.golang.org/protobuf/encoding/protojson"
 	"entgo.io/ent/dialect"
+	cerbos "github.com/cerbos/cerbos/client"
+	"context"
+	"github.com/cerbos/cerbos-go-adapters/ent-adapter/ent"
+	"strconv"
 )
 
 //go:embed testdata/query_plans.yaml
@@ -41,4 +45,66 @@ func Test_getPredicate(t *testing.T) {
 			is.Equal(tt.Args, args)
 		})
 	}
+}
+
+func TestIntegration(t *testing.T) {
+	is := require.New(t)
+	nick, simon, mary, christina, aleks := "Nick", "Simon", "Mary", "Christina", "Aleks"
+
+	tests := []struct {
+		username string
+		want     []string
+	}{
+		{
+			username: "alice",
+			want:     []string{nick, simon, mary, christina, aleks},
+		},
+		{
+			username: "john",
+			want:     []string{nick, simon, mary, aleks},
+		},
+		{
+			username: "sarah",
+			want:     []string{mary, christina, aleks, nick},
+		},
+		{
+			username: "geri",
+			want:     []string{nick, aleks},
+		},
+	}
+	cerbosAddr := "localhost:3592"
+	c, err := cerbos.New(cerbosAddr, cerbos.WithPlaintext())
+	is.NoError(err)
+	ctx := context.Background()
+	repo, err := New(ent.Log(t.Log), ent.Debug())
+	is.NoError(err)
+	err = repo.SetupDatabase(ctx)
+	is.NoError(err)
+	for _, tt := range tests {
+		t.Run(tt.username, func(t *testing.T) {
+			user, err := repo.GetUserByUsername(ctx, tt.username)
+			is.NoError(err)
+			// Create a new principal object with information from the database and the request.
+			principal := cerbos.NewPrincipal(strconv.Itoa(user.ID)).
+				WithRoles(user.Role).
+				WithAttr("department", user.Department)
+
+			queryPlan, err := c.ResourcesQueryPlan(ctx, principal, cerbos.NewResource("contact", ""), "read")
+			is.NoError(err)
+
+			filter := queryPlan.GetFilter()
+			//t.Log(protojson.Format(filter))
+			contacts, err := repo.GetContacts(ctx, filter)
+			is.NoError(err)
+			is.ElementsMatch(getNames(contacts), tt.want, tt.username)
+		})
+	}
+}
+
+func getNames(cs []*ent.Contact) []string {
+	ns := make([]string, len(cs))
+	for i := range cs {
+		ns[i] = cs[i].FirstName
+	}
+	return ns
 }
